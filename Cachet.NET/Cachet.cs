6175e229eadf79f4a12188799ed39ffc7ace08f9
Kris Franken <villainoustourist@users.noreply.github.com>
2019-01-08 20:53:46 -0600

    Basic support for reading incidents (GET requests only).

--- Cachet.NET/Cachet.cs
+++ Cachet.NET/Cachet.cs
@@ -372,5 +372,112 @@
 
             return null;
         }
+
+        /// <summary>
+        /// Gets a list of incidents from the Cachet API.
+        /// </summary>
+        public IncidentsResponse GetIncidents()
+        {
+            if (this.Initialized == false)
+            {
+                return null;
+            }
+
+            var Request = new RestRequest("incidents");
+            var Response = this.Rest.Get<IncidentsResponse>(Request);
+
+            if (Response.ResponseStatus == ResponseStatus.Completed)
+            {
+                var incidentsResponse = Response.Data;
+
+                if (incidentsResponse != null)
+                {
+                    return incidentsResponse;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the Incidents of the Cachet API.
+        /// </summary>
+        public async Task<IncidentsResponse> GetIncidentsAsync()
+        {
+            if (this.Initialized == false)
+            {
+                return null;
+            }
+
+            var Request = new RestRequest("incidents");
+            var Response = await this.Rest.ExecuteGetTaskAsync<IncidentsResponse>(Request);
+
+            if (Response.ResponseStatus == ResponseStatus.Completed)
+            {
+                var incidentsResponse = Response.Data;
+
+                if (incidentsResponse != null)
+                {
+                    return incidentsResponse;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the specified Incident of the Cachet API.
+        /// </summary>
+        /// <param name="IncidentId">The Incident identifier.</param>
+        public IncidentResponse GetIncident(int IncidentId)
+        {
+            if (this.Initialized == false)
+            {
+                return null;
+            }
+
+            var Request = new RestRequest("incidents/{incidentId}").AddUrlSegment("incidentId", IncidentId);
+            var Response = this.Rest.Get<IncidentResponse>(Request);
+
+            if (Response.ResponseStatus == ResponseStatus.Completed)
+            {
+                var IncidentResponse = Response.Data;
+
+                if (IncidentResponse != null)
+                {
+                    return IncidentResponse;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the specified Incident of the Cachet API.
+        /// </summary>
+        /// <param name="IncidentId">The Incident identifier.</param>s
+        public async Task<IncidentResponse> GetIncidentAsync(int IncidentId)
+        {
+            if (this.Initialized == false)
+            {
+                return null;
+            }
+
+            var Request = new RestRequest("incidents/{incidentId}").AddUrlSegment("incidentId", IncidentId);
+            var Response = await this.Rest.ExecuteGetTaskAsync<IncidentResponse>(Request);
+
+            if (Response.ResponseStatus == ResponseStatus.Completed)
+            {
+                var IncidentResponse = Response.Data;
+
+                if (IncidentResponse != null)
+                {
+                    return IncidentResponse;
+                }
+            }
+
+            return null;
+        }
+
     }
 }
